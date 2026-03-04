ea8b0e0ebc72c845a0f741ffd1a6b591ab1ba561
rmastodon0603 <rmastodon0603@gmail.com>
2026-03-04 20:15:42 +0200

    “Scaffold + navigation”

---
+++ TellMe/TellMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TellMe
+//
+//  Created by Volodymyr Kovalchuk on 04.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TellMe/TellMe/TellMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TellMeApp.swift
+//  TellMe
+//
+//  Created by Volodymyr Kovalchuk on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TellMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
